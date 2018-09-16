6c127a7cb4fc4d2698c19a5677f228ff7034f9a8
Eiji MATSUMOTO <e.mattsan@gmail.com>
2018-09-16 20:13:47 +0900

    read query parameters from configurations

--- lib/ex_owm/api/query.ex
+++ lib/ex_owm/api/query.ex
@@ -25,14 +25,14 @@ defmodule ExOwm.Api.Query do
 
   def init(opts) do
     %Query{
-      api_key: get_in(opts, [:api_key]),
-      city_name: get_in(opts, [:city_name]),
-      city_id: get_in(opts, [:city_id]),
-      zip_code: get_in(opts, [:zip_code]),
-      latitude: get_in(opts, [:latitude]),
-      longitude: get_in(opts, [:longitude]),
-      units: get_in(opts, [:units]),
-      language: get_in(opts, [:language])
+      api_key: get_param(opts, :api_key),
+      city_name: get_param(opts, :city_name),
+      city_id: get_param(opts, :city_id),
+      zip_code: get_param(opts, :zip_code),
+      latitude: get_param(opts, :latitude),
+      longitude: get_param(opts, :longitude),
+      units: get_param(opts, :units),
+      language: get_param(opts, :language)
     }
   end
 
@@ -45,5 +45,9 @@ defmodule ExOwm.Api.Query do
       end
     end)
     |> URI.encode_query()
+  end
+
+  defp get_param(options, key) do
+    get_in(options, [key]) || Application.get_env(:ex_owm, key)
   end
 end
